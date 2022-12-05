e4ba2c92396eb98f983a4d1ca348f35cbf07cb99
ned <github@edoardo.fyi>
2022-12-05 14:32:34 +0100

    use revchatgpt and .env

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-import json
 import logging
+import os
 
-from chat_gpt3_bot import ChatGPT3Bot
+from dotenv import load_dotenv
+from revChatGPT.revChatGPT import Chatbot as ChatGPT3Bot
 from telegram_bot import ChatGPT3TelegramBot
 
-
 def main():
+    load_dotenv()
+
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
     )
 
-    with open("config.json", "r") as f:
-        config = json.load(f)
+    chatgpt_config = {
+        'email': os.environ['OPENAI_EMAIL'],
+        'password': os.environ['OPENAI_PASSWORD'],
+    }
+    telegram_config = {
+        'telegram_bot_token': os.environ['TELEGRAM_BOT_TOKEN']
+    }
 
-    gpt3_bot = ChatGPT3Bot(config=config)
-    telegram_bot = ChatGPT3TelegramBot(config=config, gpt3_bot=gpt3_bot)
+    gpt3_bot = ChatGPT3Bot(config=chatgpt_config)
+    telegram_bot = ChatGPT3TelegramBot(config=telegram_config, gpt3_bot=gpt3_bot)
     telegram_bot.run()
 
 
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import telegram.constants
@@ -22,21 +23,31 @@ class ChatGPT3TelegramBot:
     # Reset the conversation
     async def reset(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         logging.info('Resetting the conversation...')
-        self.gpt3_bot.reset()
+        self.gpt3_bot.reset_chat()
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
 
     # React to messages
     async def prompt(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         logging.info('New message received')
         await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=telegram.constants.ChatAction.TYPING)
-        response = self.gpt3_bot.get_chat_response(update.message.text)
+        response = self.get_chatgpt_response(update.message.text)
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             reply_to_message_id=update.message.message_id,
             text=response["message"],
             parse_mode=telegram.constants.ParseMode.MARKDOWN
         )
-        logging.info('Sent response')
+
+    def get_chatgpt_response(self, message, retry=False) -> dict:
+        response = self.gpt3_bot.get_chat_response(message)
+        if isinstance(response, dict):
+            return response
+        else:
+            if not retry:
+                self.gpt3_bot.refresh_session()
+                return self.get_chatgpt_response(message, retry=True)
+            else:
+                return {"message": "I'm having some trouble talking to you, please try again later."}
 
     def run(self):
         application = ApplicationBuilder().token(self.config['telegram_bot_token']).build()
