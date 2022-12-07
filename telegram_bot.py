58be277355ee96cbc0e873a3facef3bdb584e353
ned <github@edoardo.fyi>
2022-12-07 15:55:10 +0100

    readded command handler to refresh session (#5)

--- telegram_bot.py
+++ telegram_bot.py
@@ -39,6 +39,16 @@ class ChatGPT3TelegramBot:
         self.gpt3_bot.reset_chat()
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
 
+    # Refresh session
+    async def refresh(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if not self.is_allowed(update):
+            logging.info(f'User {update.message.from_user.name} is not allowed to refresh the session')
+            return
+
+        logging.info('Refreshing session...')
+        self.gpt3_bot.refresh_session()
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
+
     # React to messages
     async def prompt(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not self.is_allowed(update):
@@ -76,8 +86,9 @@ class ChatGPT3TelegramBot:
         application.add_handler(CommandHandler('start', self.start))
         application.add_handler(CommandHandler('reset', self.reset))
         application.add_handler(CommandHandler('help', self.help))
+        application.add_handler(CommandHandler('refresh', self.refresh))
         application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.prompt))
 
         application.add_error_handler(self.error_handler)
 
-        application.run_polling(poll_interval=2.0, timeout=20)
+        application.run_polling()
