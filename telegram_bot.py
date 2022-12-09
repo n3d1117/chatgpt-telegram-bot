8b7f22c45759102eca4b23bc8b1880bd264ffe9f
nailuoGG <nailuogg@gmail.com>
2022-12-09 14:32:58 +0800

    feat: handle REPLAY message in GROUP Chat
    background:
    
    By default, all bots added to groups run in  [Privacy Mode](https://core.telegram.org/bots/features#privacy-mode)
    
    only see relevant messages and commands, see above link for detail.
    
    we get this :
    
    -  Replies to any messages implicitly or explicitly meant for this bot.
    
    Design:
    
    - use filters.REPLY
    - let bot run in privacy mode (default)
    - add a `/chat` command, send a message to group
    
    how to use:
    - user use /chat command or /chat xxxx some new text in chat
    - bot will send a message in chat
    - user reply to bot's  message

--- telegram_bot.py
+++ telegram_bot.py
@@ -135,6 +135,11 @@ class ChatGPT3TelegramBot:
         application.add_handler(CommandHandler('reset', self.reset))
         application.add_handler(CommandHandler('help', self.help))
         application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.prompt))
+        application.add_handler(
+            MessageHandler(
+                (filters.TEXT | filters.REPLAY) & (~filters.COMMAND), self.prompt
+            )
+        )
 
         application.add_error_handler(self.error_handler)
 
