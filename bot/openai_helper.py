e43103a838805764b825907a05c68e3ef8bdb0f3
ned <github@edoardo.fyi>
2023-03-28 13:59:46 +0200

    update token counting function

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -255,7 +255,7 @@ class OpenAIHelper:
             encoding = tiktoken.get_encoding("gpt-3.5-turbo")
 
         if model in GPT_3_MODELS:
-            tokens_per_message = 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+            tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
             tokens_per_name = -1  # if there's a name, the role is omitted
         elif model in GPT_4_MODELS + GPT_4_32K_MODELS:
             tokens_per_message = 3
@@ -269,7 +269,7 @@ class OpenAIHelper:
                 num_tokens += len(encoding.encode(value))
                 if key == "name":
                     num_tokens += tokens_per_name
-        num_tokens += 2  # every reply is primed with <im_start>assistant
+        num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
         return num_tokens
 
     def get_grant_balance(self):
