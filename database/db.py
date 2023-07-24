ab6b10b1bd6564f8433911c8bfcedc1169c1ac53
ToReRo091 <65923708+ToReRo091@users.noreply.github.com>
2023-07-24 20:20:30 +0600

    add db.py
    -- Создаем экземпляр класса Database
    from database.db import Database
    db = Database()
    -- Пример использования
    query = "SELECT user_id FROM users WHERE user_id = %s"
    db.fetch_one(query, (str(message.from_user.id),))
    -- больше информации https://ploshadka.net/primer-zaprosov-na-psycopg2/
    -- Не забыть потом крендешиалы перенести в переменную окружения

---
+++ database/db.py
@@ -0,0 +1,102 @@
+import psycopg2
+from psycopg2.extras import NamedTupleCursor
+
+
+class Database:
+    def __init__(self):
+        self.connection = psycopg2.connect(
+            "user=user password=pass host=localhost port=5477 dbname=name_db")
+        self.connection.autocommit = True
+
+    def fetch_one(self, query, arg=None, factory=None, clean=None):
+        """ Получает только одно ЕДИНСТВЕННОЕ значение (не ряд!) из таблицы
+        :param query: Запрос
+        :param arg: Переменные
+        :param factory: dic (возвращает словарь - ключ/значение) или list (возвращает list)
+        :param clean: С параметром вернет только значение. Без параметра вернет значение в кортеже.
+        """
+        try:
+            cur = self.__connection(factory)
+            self.__execute(cur, query, arg)
+            return self.__fetch(cur, clean)
+
+        except (Exception, psycopg2.Error) as error:
+            self.__error(error)
+
+    def fetch_all(self, query, arg=None, factory=None):
+        """ Получает множетсвенные данные из таблицы
+        :param query: Запрос
+        :param arg: Переменные
+        :param factory: dic (возвращает словарь - ключ/значение) или list (возвращает list)
+        """
+        try:
+            cur = self.__connection(factory)
+            self.__execute(cur, query, arg)
+            return cur.fetchall()
+
+        except (Exception, psycopg2.Error) as error:
+            self.__error(error)
+
+    def query_update(self, query, arg, message=None):
+        """ Обновляет данные в таблице и возвращает сообщение об успешной операции """
+        try:
+            cur = self.connection.cursor()
+            cur.execute(query, arg)
+            return message
+
+        except (Exception, psycopg2.Error) as error:
+            self.__error(error)
+
+    def close(self):
+        cur = self.connection.cursor()
+        cur.close()
+        self.connection.close()
+
+    def __connection(self, factory=None):
+        # Dic - возвращает словарь - ключ/значение
+        if factory == 'dic':
+            cur = self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        # List - возвращает list (хотя и называется DictCursor)
+        elif factory == 'list':
+            cur = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        # Tuple
+        else:
+            cur = self.connection.cursor()
+
+        return cur
+
+    @staticmethod
+    def __execute(cur, query, arg=None):
+        # Метод 'execute' всегда возвращает None
+        if arg:
+            cur.execute(query, arg)
+        else:
+            cur.execute(query)
+
+    @staticmethod
+    def __fetch(cur, clean):
+        # Если запрос был выполнен успешно, получим данные с помощью 'fetchone'
+        if clean == 'no':
+            # Вернет:
+            #   Название:
+            #       ('Royal Caribbean Cruises',)
+            #   Дата:
+            #       (datetime.datetime(2020, 6, 2, 13, 36, 35, 61052, tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=0, name=None)),)
+            fetch = cur.fetchone()
+        else:
+            # Вернет:
+            #   Название:
+            #       Royal Caribbean Cruises
+            #   Дата:
+            #       (da2020-06-02 13:36:35.061052+00:00
+            fetch = cur.fetchone()[0]
+        return fetch
+
+    @staticmethod
+    def __error(error):
+        # В том числе, если в БД данных нет, будет ошибка на этапе fetchone
+        print('Данных в бд нет или ошибка: {}'.format(error))
+        return None
+
