900d40c65fcbea2d874f604f029644b4fca727ed
AlexHTW <alexander.hinzer@htw-berlin.de>
2023-11-07 00:05:04 +0100

    removed vision model

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -22,7 +22,7 @@ GPT_3_MODELS = ("gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613")
 GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613")
 GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613")
 GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
-GPT_4_128K_MODELS = ("gpt-4-1106-preview", "gpt-4-vision-preview")
+GPT_4_128K_MODELS = ("gpt-4-1106-preview")
 GPT_ALL_MODELS = GPT_3_MODELS + GPT_3_16K_MODELS + GPT_4_MODELS + GPT_4_32K_MODELS + GPT_4_128K_MODELS
 
 
@@ -53,7 +53,7 @@ def are_functions_available(model: str) -> bool:
     if model in ("gpt-3.5-turbo-0301", "gpt-4-0314", "gpt-4-32k-0314"):
         return False
     # Stable models will be updated to support functions on June 27, 2023
-    if model in ("gpt-3.5-turbo", "gpt-4", "gpt-4-32k","gpt-4-1106-preview", "gpt-4-vision-preview"):
+    if model in ("gpt-3.5-turbo", "gpt-4", "gpt-4-32k","gpt-4-1106-preview"):
         return datetime.date.today() > datetime.date(2023, 6, 27)
     return True
 
