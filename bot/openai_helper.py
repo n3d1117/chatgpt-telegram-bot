21d7f1f70572e00417af5b1c8ff77221905795c2
AlexHTW <alexander.hinzer@htw-berlin.de>
2023-11-07 01:11:44 +0100

    changed available sizes for dall-e-3

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -22,7 +22,7 @@ GPT_3_MODELS = ("gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613")
 GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613")
 GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613")
 GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
-GPT_4_128K_MODELS = ("gpt-4-1106-preview")
+GPT_4_128K_MODELS = ("gpt-4-1106-preview",)
 GPT_ALL_MODELS = GPT_3_MODELS + GPT_3_16K_MODELS + GPT_4_MODELS + GPT_4_32K_MODELS + GPT_4_128K_MODELS
 
 
