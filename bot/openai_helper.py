968c3b94df7133e171b4d50439685ab697087133
AlexHTW <alexander.hinzer@htw-berlin.de>
2023-11-07 22:29:31 +0100

    fixed model name

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -38,7 +38,7 @@ def default_max_tokens(model: str) -> int:
     elif model in GPT_4_MODELS:
         return base * 2
     elif model in GPT_3_16K_MODELS:    
-        if model == "gpt-3.5-turbo-0301":
+        if model == "gpt-3.5-turbo-1106":
             return 4096
         return base * 4
     elif model in GPT_4_32K_MODELS:
