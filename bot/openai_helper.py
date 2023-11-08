086f8447376b3faa27631bfe13b654fd54223757
gilcu3 <828241+gilcu3@users.noreply.github.com>
2023-11-08 19:10:11 +0100

    vision model do not seem to support function calling

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -57,6 +57,8 @@ def are_functions_available(model: str) -> bool:
     # Stable models will be updated to support functions on June 27, 2023
     if model in ("gpt-3.5-turbo", "gpt-4", "gpt-4-32k"):
         return datetime.date.today() > datetime.date(2023, 6, 27)
+    if model == 'gpt-4-vision-preview':
+        return False
     return True
 
 
