6024da281760692f25b70e3062d5b39c4f8b10f9
k3it <gokoyev@gmail.com>
2023-11-18 21:46:33 -0500

    Revert "Add gpt-4-1106-preview to GPT_4_MODELS"
    This reverts commit dcfe47e478d2b60edad42aa5d686308fe4545be0.

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -20,7 +20,7 @@ from plugin_manager import PluginManager
 # Models can be found here: https://platform.openai.com/docs/models/overview
 GPT_3_MODELS = ("gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613")
 GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613")
-GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613", "gpt-4-1106-preview")
+GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613")
 GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
 GPT_ALL_MODELS = GPT_3_MODELS + GPT_3_16K_MODELS + GPT_4_MODELS + GPT_4_32K_MODELS
 
