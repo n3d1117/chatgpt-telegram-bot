6bce7272ee12f3ee1d40926d933022a0186af041
Matt <Geczy@users.noreply.github.com>
2024-02-19 10:31:51 -0600

    try

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -22,14 +22,12 @@ from plugin_manager import PluginManager
 
 # Models can be found here: https://platform.openai.com/docs/models/overview
 GPT_3_MODELS = (
-    "pplx-7b-chat",
-    "pplx-7b-online",
     "gpt-3.5-turbo",
     "gpt-3.5-turbo-0301",
     "gpt-3.5-turbo-0613",
 )
 GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-1106")
-GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613")
+GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613", "pplx-7b-chat", "pplx-7b-online")
 GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
 GPT_4_VISION_MODELS = ("gpt-4-vision-preview",)
 GPT_4_128K_MODELS = ("gpt-4-1106-preview",)
