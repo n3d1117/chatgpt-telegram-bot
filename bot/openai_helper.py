01f4e23fd03eb755c7e14d754ec139b72df1ac14
Matt Gates <mgates@hubspot.com>
2024-03-01 11:02:07 -0600

    add sonar-small-online

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -27,7 +27,14 @@ GPT_3_MODELS = (
     "gpt-3.5-turbo-0613",
 )
 GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-1106")
-GPT_4_MODELS = ("gpt-4", "gpt-4-0314", "gpt-4-0613", "pplx-7b-chat", "pplx-7b-online")
+GPT_4_MODELS = (
+    "gpt-4",
+    "gpt-4-0314",
+    "gpt-4-0613",
+    "pplx-7b-chat",
+    "sonar-small-online",
+    "pplx-7b-online",
+)
 GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613")
 GPT_4_VISION_MODELS = ("gpt-4-vision-preview",)
 GPT_4_128K_MODELS = ("gpt-4-1106-preview",)
