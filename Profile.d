d9470a5470ca97297255bc5569c68677fe15b7ee
RoyalKatsic1 <malingamark30@gmail.com>
2024-06-20 04:52:41 +0300

    Update Profile.d

--- Profile.d
+++ Profile.d
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+
+# Set the PORT environment variable (important!)
+export PORT=10000  # Replace 10000 with your desired/assigned port
+
+# Start your Gunicorn process in the background
+gunicorn bot.main:app --bind 0.0.0.0:$PORT & 
