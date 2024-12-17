7a8acdd055e1cad2e84119f8879167d99e572b88
SkySlider <38480571+SkySlider@users.noreply.github.com>
2024-12-17 23:23:40 +0300

    Update openai_helper.py
    "proxies" deprecated since httpx==0.28.0, replaced with "proxy"

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -113,7 +113,7 @@ class OpenAIHelper:
         :param config: A dictionary containing the GPT configuration
         :param plugin_manager: The plugin manager
         """
-        http_client = httpx.AsyncClient(proxies=config['proxy']) if 'proxy' in config else None
+        http_client = httpx.AsyncClient(proxy=config['proxy']) if 'proxy' in config else None
         self.client = openai.AsyncOpenAI(api_key=config['api_key'], http_client=http_client)
         self.config = config
         self.plugin_manager = plugin_manager
